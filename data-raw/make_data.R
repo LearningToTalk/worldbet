2ae6e12ae0399f0952ba2fce82685b0efd957c33
TJ Mahr <mahr@wisc.edu>
2015-09-16 18:45:29 -0500

    export worldbet_key

--- data-raw/make_data.R
+++ data-raw/make_data.R
@@ -1,4 +1,5 @@
-# Generate a data object from the translation table
+# Generate data objects from the translation table
+library("stringr")
 
 worldbet_key <- read.table(
   "data-raw/worldbet.txt",
@@ -10,4 +11,14 @@ worldbet_key <- read.table(
   comment.char = ""
 )
 
-devtools::use_data(worldbet_key, diacritics, segmentals, segmentals_to_ipa, internal = TRUE, overwrite = TRUE)
+# Main dataframe for symbol conversions
+devtools::use_data(worldbet_key, overwrite = TRUE)
+
+# Load the escape_regex function
+devtools::load_all()
+
+# Package-internal chunks of data
+diacritics <- worldbet_key[str_detect(worldbet_key$WorldBet, "_"), ]
+segmentals <- worldbet_key[!str_detect(worldbet_key$WorldBet, "_"), ]
+segmentals_to_ipa <- structure(segmentals$IPA, names = escape_regex(segmentals$WorldBet))
+devtools::use_data(diacritics, segmentals, segmentals_to_ipa, internal = TRUE, overwrite = TRUE)
