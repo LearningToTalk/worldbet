e0c0e16d5e9a92601e7d90882ac55876fdfc802c
unknown <mahr@wisc.edu>
2015-09-17 11:25:48 -0500

    Update data-set documentation

--- R/worldbet_key.R
+++ R/worldbet_key.R
@@ -1,8 +1,8 @@
 #' WorldBet symbol key
 #'
-#' A table containing different representations of IPA symbols.
+#' A table containing different representations of (English) IPA symbols.
 #'
-#' @format A data frame with 41 rows and 3 variables:
+#' @format A data frame with 42 rows and 3 variables:
 #' \describe{
 #'   \item{WorldBet}{ASCII representation of the symbol}
 #'   \item{IPA}{IPA symbol, encoded in UTF-8}
