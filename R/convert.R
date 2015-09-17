bb1e3fee2b920f52f159d20fdcce15f8d4b166e3
unknown <mahr@wisc.edu>
2015-09-17 11:28:23 -0500

    don't use rewrite-rules. they don't work with HTML entities.
    instead recursively break a string into worldbet tokens, then look-up the IPA/HTML from a table.
    
    support for html entities

--- R/convert.R
+++ R/convert.R
@@ -9,15 +9,73 @@ escape_regex <- function(xs) {
 
 
 #' Convert a Worldbet string to IPA
+#' @param xs a character vector of WorldBet strings
+#' @return a character vector of Unicode IPA symbols
 #' @export
-convert_bet_to_ipa <- function(bet) {
-  # First convert diacritics, then segmentals
-  bet %>%
-    str_replace_all(bet_to_ipa$accented) %>%
-    str_replace_all(bet_to_ipa$diacritics) %>%
-    str_replace_all(bet_to_ipa$segments)
+#' @examples
+#' convert_bet_to_ipa("TIn")
+#' #> [1] "θɪn"
+#' convert_bet_to_ipa(c("TIn", "SUg&_r"))
+#' #> [1] "θɪn"  "ʃʊgɚ"
+convert_bet_to_ipa <- function(xs) {
+  vapply(xs, from_bet_to_ipa, character(1), USE.NAMES = FALSE)
 }
 
-# convert_bet_to_html <- function(bet) {
-#   str_replace_all(bet, segmentals_to_ipa)
-# }
+#' Convert a Worldbet string to HTML entities
+#' @inheritParams convert_bet_to_ipa
+#' @return a character vector of Unicode IPA symbols
+#' @export
+#' @examples
+#' convert_bet_to_entity("TIn")
+#' #> [1] "θɪn"
+#' convert_bet_to_ipa(c("TIn", "SUg&_r"))
+#' #> [1] "θɪn"  "ʃʊgɚ"
+convert_bet_to_entity <- function(xs) {
+  vapply(xs, from_bet_to_entity, character(1), USE.NAMES = FALSE)
+}
+
+# Create a function that converts one symbol from one representation to another
+make_converter <- function(tokens, lookup) {
+  function(x) {
+    chunks <- str_tokenize(x, tokens) %>% str_drop_blanks
+    str_collapse(lookup[chunks])
+  }
+}
+
+from_bet_to_ipa <- make_converter(names(to_ipa), to_ipa)
+from_bet_to_entity <- make_converter(names(to_entity), to_entity)
+
+
+
+
+# Collapse a vector of strings into a single string
+str_collapse <- function(xs) paste0(xs, collapse = "")
+
+# Extract first character of a string
+str_onset <- function(x) str_extract(x, "^.")
+
+# Find longest string
+str_longest <- function(xs) xs[which.max(nchar(xs))]
+
+# Remove empty strings
+str_drop_blanks <- function(xs) xs[xs != ""]
+
+# Break a string into a vector of predefined tokens
+str_tokenize <- function(x, tokens) chomp_tokens("", x, tokens)
+
+# Recursively remove tokens from the onset of a string
+chomp_tokens <- function(left, right, tokens) {
+  # Find the largest matching chunk from the start of the word
+  first <- str_onset(right) %>% escape_regex
+  continuations <- str_subset(tokens, paste0("^", first))
+  bite <- str_longest(continuations)
+
+  # Bite off that chunk from the string
+  leftover <- str_replace(right, escape_regex(bite), "") %>% str_drop_blanks
+  left <- c(left, bite)
+  right <- leftover
+
+  # Repeat if there's more to consume
+  if (length(leftover) != 0) chomp_tokens(left, leftover, tokens) else left
+}
+
--- data-raw/make_data.R
+++ data-raw/make_data.R
@@ -1,5 +1,6 @@
 # Generate data objects from the translation table
 library("stringr")
+library("dplyr", warn.conflicts = FALSE)
 
 worldbet_key <- read.table(
   "data-raw/worldbet.txt",
@@ -11,30 +12,11 @@ worldbet_key <- read.table(
   comment.char = ""
 )
 
-# Main dataframe for symbol conversions
+# Package up dataframe used for symbol conversions
 devtools::use_data(worldbet_key, overwrite = TRUE)
 
-# Load the escape_regex function
-devtools::load_all()
+# Package-internal lookup tables
+to_ipa <- setNames(worldbet_key$IPA, worldbet_key$WorldBet)
+to_entity <- setNames(worldbet_key$Entity, worldbet_key$WorldBet)
 
-negate = TRUE
-
-# Create a named vector of string-rewrite rules for converting worldbet to ipa
-prep_ipa <- function(pattern, negate) {
-  f <- if (negate) Negate(str_detect) else str_detect
-  hits <- worldbet_key[f(worldbet_key$WorldBet, pattern), ]
-  setNames(hits$IPA, escape_regex(hits$WorldBet))
-}
-
-# Package-internal chunks of data
-bet_to_ipa <- list(
-  # WorldBet items with inherent diacritics: X_X
-  accented = prep_ipa("._.", negate = FALSE),
-  # WorldBet diacritics: _X
-  diacritics = prep_ipa("^_", negate = FALSE),
-  # Plain old segments (anything without an underscore)
-  segments = prep_ipa(pattern = "_", negate = TRUE)
-)
-
-
-devtools::use_data(bet_to_ipa, internal = TRUE, overwrite = TRUE)
+devtools::use_data(to_ipa, to_entity, internal = TRUE, overwrite = TRUE)
