c76df9ead86aa943509adca0819895979c5e6057
TJ Mahr <mahr@wisc.edu>
2015-09-16 20:28:20 -0500

    refactor data-prepping script

--- R/convert.R
+++ R/convert.R
@@ -11,5 +11,13 @@ escape_regex <- function(xs) {
 #' Convert a Worldbet string to IPA
 #' @export
 convert_bet_to_ipa <- function(bet) {
-  str_replace_all(bet, segmentals_to_ipa)
+  # First convert diacritics, then segmentals
+  bet %>%
+    str_replace_all(bet_to_ipa$accented) %>%
+    str_replace_all(bet_to_ipa$diacritics) %>%
+    str_replace_all(bet_to_ipa$segments)
 }
+
+# convert_bet_to_html <- function(bet) {
+#   str_replace_all(bet, segmentals_to_ipa)
+# }
--- data-raw/make_data.R
+++ data-raw/make_data.R
@@ -17,8 +17,24 @@ devtools::use_data(worldbet_key, overwrite = TRUE)
 # Load the escape_regex function
 devtools::load_all()
 
+negate = TRUE
+
+# Create a named vector of string-rewrite rules for converting worldbet to ipa
+prep_ipa <- function(pattern, negate) {
+  f <- if (negate) Negate(str_detect) else str_detect
+  hits <- worldbet_key[f(worldbet_key$WorldBet, pattern), ]
+  setNames(hits$IPA, escape_regex(hits$WorldBet))
+}
+
 # Package-internal chunks of data
-diacritics <- worldbet_key[str_detect(worldbet_key$WorldBet, "_"), ]
-segmentals <- worldbet_key[!str_detect(worldbet_key$WorldBet, "_"), ]
-segmentals_to_ipa <- structure(segmentals$IPA, names = escape_regex(segmentals$WorldBet))
-devtools::use_data(diacritics, segmentals, segmentals_to_ipa, internal = TRUE, overwrite = TRUE)
+bet_to_ipa <- list(
+  # WorldBet items with inherent diacritics: X_X
+  accented = prep_ipa("._.", negate = FALSE),
+  # WorldBet diacritics: _X
+  diacritics = prep_ipa("^_", negate = FALSE),
+  # Plain old segments (anything without an underscore)
+  segments = prep_ipa(pattern = "_", negate = TRUE)
+)
+
+
+devtools::use_data(bet_to_ipa, internal = TRUE, overwrite = TRUE)
